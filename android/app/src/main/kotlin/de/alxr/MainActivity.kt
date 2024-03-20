9a501220b7d3fbea5cdf517d6ed11851be46f180
alxr@lenovo <mail@alxr.de>
2024-03-20 16:19:57 +0100

    Init with very good cli

---
+++ android/app/src/main/kotlin/de/alxr/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.alxr.barcodes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:barcodes/app/app.dart';
+import 'package:barcodes/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
